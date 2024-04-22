265857f13cee5fe90f355ba9c7207f0d9f0ba0ab
EmileSean <emilesean.es@gmail.com>
2024-04-22 11:06:01 +0300

    Add swaping on uniswap v2

---
+++ test/IUniswapV2Router.t.sol
@@ -0,0 +1,254 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "forge-std/interfaces/IERC20.sol";
+import {IUniswapV2Factory} from "./interfaces/IUniswapV2Factory.sol";
+import {IUniswapV2Router} from "./interfaces/IUniswapV2Router.sol";
+import {IUniswapV2Pair} from "./interfaces/IUniswapV2Pair.sol";
+
+contract IUniswapV2SwapExamples {
+    IUniswapV2Factory public factory =
+        IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
+    IUniswapV2Router public router =
+        IUniswapV2Router(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+
+    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address private constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+
+    IERC20 private weth = IERC20(WETH);
+    IERC20 private dai = IERC20(DAI);
+
+    // function setUp() public {
+    //     vm.createSelectFork("mainnet", 19_708_589);
+    //     // vm.rollFork(10000835 + 1); // Creation date of UniswapV2Factory
+    // }
+    function swapSingleHopExactAmountIn(
+        uint256 amountIn,
+        uint256 amountOutMin
+    ) external returns (uint256 amountOut) {
+        weth.transferFrom(msg.sender, address(this), amountIn);
+        weth.approve(address(router), amountIn);
+
+        address[] memory path;
+        path = new address[](2);
+        path[0] = WETH;
+        path[1] = DAI;
+
+        uint256[] memory amounts = router.swapExactTokensForTokens(
+            amountIn,
+            amountOutMin,
+            path,
+            msg.sender,
+            block.timestamp
+        );
+        // uint256 daiamount = amounts[1];
+        // dai.transfer(address(this), daiamount);
+
+        // amounts[0] = WETH amount, amounts[1] = DAI amount
+        return amounts[1];
+    }
+
+    // Swap DAI -> WETH -> USDC
+    function swapMultiHopExactAmountIn(
+        uint256 amountIn,
+        uint256 amountOutMin
+    ) external returns (uint256 amountOut) {
+        dai.transferFrom(msg.sender, address(this), amountIn);
+        dai.approve(address(router), amountIn);
+
+        address[] memory path;
+        path = new address[](3);
+        path[0] = DAI;
+        path[1] = WETH;
+        path[2] = USDC;
+
+        uint256[] memory amounts = router.swapExactTokensForTokens(
+            amountIn,
+            amountOutMin,
+            path,
+            msg.sender,
+            block.timestamp
+        );
+
+        // amounts[0] = DAI amount
+        // amounts[1] = WETH amount
+        // amounts[2] = USDC amount
+        return amounts[2];
+    }
+
+    // Swap WETH to DAI
+    function swapSingleHopExactAmountOut(
+        uint256 amountOutDesired,
+        uint256 amountInMax
+    ) external returns (uint256 amountOut) {
+        weth.transferFrom(msg.sender, address(this), amountInMax);
+        weth.approve(address(router), amountInMax);
+
+        address[] memory path;
+        path = new address[](2);
+        path[0] = WETH;
+        path[1] = DAI;
+
+        uint256[] memory amounts = router.swapTokensForExactTokens(
+            amountOutDesired,
+            amountInMax,
+            path,
+            msg.sender,
+            block.timestamp
+        );
+
+        // Refund WETH to msg.sender
+        if (amounts[0] < amountInMax) {
+            weth.transfer(msg.sender, amountInMax - amounts[0]);
+        }
+
+        return amounts[1];
+    }
+
+    // Swap DAI -> WETH -> USDC
+    function swapMultiHopExactAmountOut(
+        uint256 amountOutDesired,
+        uint256 amountInMax
+    ) external returns (uint256 amountOut) {
+        dai.transferFrom(msg.sender, address(this), amountInMax);
+        dai.approve(address(router), amountInMax);
+
+        address[] memory path;
+        path = new address[](3);
+        path[0] = DAI;
+        path[1] = WETH;
+        path[2] = USDC;
+
+        uint256[] memory amounts = router.swapTokensForExactTokens(
+            amountOutDesired,
+            amountInMax,
+            path,
+            msg.sender,
+            block.timestamp
+        );
+
+        // Refund DAI to msg.sender
+        if (amounts[0] < amountInMax) {
+            dai.transfer(msg.sender, amountInMax - amounts[0]);
+        }
+
+        return amounts[2];
+    }
+}
+interface IWETH is IERC20 {
+    function deposit() external payable;
+    function withdraw(uint256 amount) external;
+}
+contract UniswapV2SwapExamplesTest is Test {
+    address public alice = vm.addr(12);
+    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+
+    IWETH private weth = IWETH(WETH);
+    IERC20 private dai = IERC20(DAI);
+    IERC20 private usdc = IERC20(USDC);
+    IUniswapV2SwapExamples private uni;
+
+    function setUp() public {
+        vm.createSelectFork("mainnet", 19_708_589);
+        // vm.startPrank(alice);
+        uni = new IUniswapV2SwapExamples();
+    }
+
+    // Swap WETH -> DAI
+    function testSwapSingleHopExactAmountIn() public {
+        vm.startPrank(alice);
+        vm.deal(alice, 1000 ether);
+        weth.deposit{value: 1 ether}();
+        weth.approve(address(uni), 1 ether);
+        console.log("Alice WETH balance %s", weth.balanceOf(alice));
+        console.log("Alice DAI balance %s", dai.balanceOf(alice));
+
+        uint256 amountOutMin = 1;
+        uint256 amountIn = 1 ether;
+        uint256 daiAmountOut = uni.swapSingleHopExactAmountIn(
+            amountIn,
+            amountOutMin
+        );
+        daiAmountOut;
+
+        console.log("Alice WETH balance %s", weth.balanceOf(alice));
+        console.log("Alice DAI balance %s", dai.balanceOf(alice));
+    }
+
+    // Swap DAI -> WETH -> USDC
+    function testSwapMultiHopExactAmountIn() public {
+        // Swap WETH -> DAI
+        uint256 wethAmount = 1e18;
+        weth.deposit{value: wethAmount}();
+        weth.approve(address(uni), wethAmount);
+
+        uint256 daiAmountMin = 1;
+        uni.swapSingleHopExactAmountIn(wethAmount, daiAmountMin);
+
+        // Swap DAI -> WETH -> USDC
+        uint256 daiAmountIn = 1e18;
+        dai.approve(address(uni), daiAmountIn);
+
+        uint256 usdcAmountOutMin = 1;
+        uint256 usdcAmountOut = uni.swapMultiHopExactAmountIn(
+            daiAmountIn,
+            usdcAmountOutMin
+        );
+
+        console2.log("USDC", usdcAmountOut);
+        assertGe(usdcAmountOut, usdcAmountOutMin, "amount out < min");
+    }
+
+    // Swap WETH -> DAI
+    function testSwapSingleHopExactAmountOut() public {
+        uint256 wethAmount = 1e18;
+        weth.deposit{value: wethAmount}();
+        weth.approve(address(uni), wethAmount);
+
+        uint256 daiAmountDesired = 1e18;
+        uint256 daiAmountOut = uni.swapSingleHopExactAmountOut(
+            daiAmountDesired,
+            wethAmount
+        );
+
+        console2.log("DAI", daiAmountOut);
+        assertEq(
+            daiAmountOut,
+            daiAmountDesired,
+            "amount out != amount out desired"
+        );
+    }
+
+    // Swap DAI -> WETH -> USDC
+    function testSwapMultiHopExactAmountOut() public {
+        // Swap WETH -> DAI
+        uint256 wethAmount = 1e18;
+        weth.deposit{value: wethAmount}();
+        weth.approve(address(uni), wethAmount);
+
+        // Buy 100 DAI
+        uint256 daiAmountOut = 100 * 1e18;
+        uni.swapSingleHopExactAmountOut(daiAmountOut, wethAmount);
+
+        // Swap DAI -> WETH -> USDC
+        dai.approve(address(uni), daiAmountOut);
+
+        uint256 amountOutDesired = 1e6;
+        uint256 amountOut = uni.swapMultiHopExactAmountOut(
+            amountOutDesired,
+            daiAmountOut
+        );
+
+        console2.log("USDC", amountOut);
+        assertEq(
+            amountOut,
+            amountOutDesired,
+            "amount out != amount out desired"
+        );
+    }
+}
